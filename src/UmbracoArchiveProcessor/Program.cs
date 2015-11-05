e8e647fe89c6ca79473cb8fbb13ec917cc5cbb9f
leekelleher <leekelleher@gmail.com>
2015-11-05 22:53:40 +0000

    Downloads the latest release archive of Umbraco.

--- src/UmbracoArchiveProcessor/Program.cs
+++ src/UmbracoArchiveProcessor/Program.cs
@@ -1,22 +1,34 @@
 ﻿using System;
 using System.IO;
+using System.Net;
+using System.Threading.Tasks;
 using HtmlAgilityPack;
 
 namespace UmbracoArchiveProcessor
 {
 	class Program
 	{
+		static readonly object _object = new object();
+
 		static void Main(string[] args)
 		{
-			Console.WriteLine(Environment.CurrentDirectory);
+			var current_dir = Environment.CurrentDirectory;
 
+			var target_path = Path.Combine(current_dir, "artifacts");
+			var target_dir = new DirectoryInfo(target_path);
 
-			var umbraco_version = DiscoverLatestRelease();
-			
+			if (!target_dir.Exists)
+				target_dir.Create();
+
+			var umbraco_version = GetLatestUmbracoVersionNumber();
+
+			if (!string.IsNullOrWhiteSpace(umbraco_version))
+				DownloadUmbracoReleaseArchive(umbraco_version, target_path);
+
 			Console.WriteLine(umbraco_version);
 		}
 
-		static string DiscoverLatestRelease()
+		static string GetLatestUmbracoVersionNumber()
 		{
 			var web = new HtmlWeb();
 
@@ -26,7 +38,50 @@ namespace UmbracoArchiveProcessor
 			var link = doc.DocumentNode.SelectSingleNode("//a[@id='downloadButton']/span[1]");
 			var text = link.InnerText;
 
-			return text.Replace("Download Umbraco v", string.Empty);
+			var version_number = text.Replace("Download Umbraco v", string.Empty);
+
+			Console.WriteLine("Latest Umbraco version number: {0}", version_number);
+
+			return version_number;
+		}
+
+		static void DownloadUmbracoReleaseArchive(string umbraco_version, string target_path)
+		{
+			var filename = string.Format("UmbracoCms.{0}.zip", umbraco_version);
+
+			if (File.Exists(Path.Combine(target_path, filename)))
+			{
+				Console.WriteLine("File already downloaded.");
+				return;
+			}
+
+			var web = new HtmlWeb();
+
+			using (var client = new WebClient())
+			{
+				var filepath = Path.Combine(target_path, filename);
+				var uri = new Uri(string.Format("http://umbracoreleases.blob.core.windows.net/download/UmbracoCms.{0}.zip", umbraco_version));
+				var currentCursorTop = Console.CursorTop;
+
+				client.DownloadProgressChanged += (s, e) =>
+				{
+					lock (_object)
+					{
+						Console.SetCursorPosition(0, currentCursorTop);
+						Console.Write("{0} downloaded {1} of {2} bytes. {3}% complete...",
+							((TaskCompletionSource<object>)e.UserState).Task.AsyncState,
+							e.BytesReceived,
+							e.TotalBytesToReceive,
+							e.ProgressPercentage);
+					}
+				};
+
+				var task = client.DownloadFileTaskAsync(uri, filepath);
+
+				task.Wait();
+			}
+
+			Console.WriteLine("\n\rDownload complete.");
 		}
 	}
 }
