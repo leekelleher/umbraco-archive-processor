566113240d8e7703d60811eed7b313ca8dec093e
leekelleher <leekelleher@gmail.com>
2015-11-05 22:17:24 +0000

    Updated appveyor.yml
    Added bootstrapper console app.

---
+++ src/UmbracoArchiveProcessor/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace UmbracoArchiveProcessor
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Testing Console.WriteLine");
+			Trace.WriteLine("Testing Trace.WriteLine");
+		}
+	}
+}
---
+++ src/UmbracoArchiveProcessor/Properties/AssemblyInfo.cs
@@ -0,0 +1,16 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Umbraco Archive Processor")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Umbraco Archive Processor")]
+[assembly: AssemblyCopyright("Copyright \xa9 Lee Kelleher 2015")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+[assembly: Guid("ECCE54FA-7D6B-41CA-85D4-545B5C947853")]
+
+[assembly: AssemblyVersion("1.0.*")]
